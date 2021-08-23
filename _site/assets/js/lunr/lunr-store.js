5f75a91a1848adce7377bd04da39313bfe0e3881
Song Changan <casong99@gmail.com>
2021-08-23 11:03:15 +0900

    Theme changed

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "First Post",
-        "excerpt":"Front Matter   date: 2019-01-25 title: “Jekyll 첫번째 글” categories: blog tags: jekyll  목차  toc: true  toc_sticky: true  —  ","categories": [],
-        "tags": [],
-        "url": "/first-post/",
-        "teaser": null
-      }]
